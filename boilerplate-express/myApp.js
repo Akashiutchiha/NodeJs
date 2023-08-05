1b9d12fce278fe398b89be6f1fbaf64e97a1eb9f
Akashiutchiha <christianhonore2003@gmail.com>
2023-08-05 18:00:44 +0100

    Sent a static folder an putting a css file inside

--- boilerplate-express/myApp.js
+++ boilerplate-express/myApp.js
@@ -2,11 +2,16 @@ let express = require('express');
 let app = express();
 
 app.get("/", function(req, res) {
+    // sending a text message to the browser
+    // res.send("Hello Express");
 
-    res.send("Hello Express");
-
+    // sending a file to the browser
     absolutePath = __dirname + '/views/index.html';
     res.sendFile(absolutePath);
+
+    // sending a static folder an putting a css file insde
+    app.use("/public", express.static(__dirname + "/public"));
+
 });
 
 
